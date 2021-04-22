68fd8325f33010fe105fd434bdd06c08f922c8ee
unstoppableCoderAdi <80784437+unstoppableCoderAdi@users.noreply.github.com>
2021-04-22 17:54:29 +0530

    Add files via upload

---
+++ ALGORITHMS/2.Recursive Digit Sum.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int superd(string s){
+    if(s.length()==1) return s[0]-'0';
+    else{
+        int ans=0;
+        for(int i=0;i<s.length();++i){
+            ans += s[i]-'0';
+        }
+        return superd(to_string(ans));
+    }
+    return 0;
+}
+
+// Complete the superDigit function below.
+int superDigit(string n, int k) {
+    return superd(to_string(k*superd(n)));
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    string n = nk[0];
+
+    int k = stoi(nk[1]);
+
+    int result = superDigit(n, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
